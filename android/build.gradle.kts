0f6040f9a549077960085d58990e4d426bb4cf22
eduardorobles969 <eduardorobles969@gmail.com>
2025-08-07 10:26:11 -0700

    actualizacion

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     // otros plugins...
-    id("com.google.gms.google-services") version "4.4.15" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.15")
+        classpath("com.google.gms:google-services:4.4.0")
     }
 }
 
